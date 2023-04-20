0221f3459b7ca4397022a6c3af547a6161118965
Warrior1119 <warriorjhs@outlook.com>
2023-04-20 12:00:37 -0400

    add provider

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
 
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
